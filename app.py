26b18f9a1686394b235b3000b420617ace349ee3
didierrc <didieryamil02@gmail.com>
2025-03-04 15:41:58 +0100

    Changed app to follow creation pattern and initialise from cofig

--- app.py
+++ app.py
@@ -1,30 +1,49 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from kusibot.config import config
+from kusibot.database.db import init_db
 from kusibot.api.main.routes import main_bp
 from kusibot.api.auth.routes import auth_bp
 from kusibot.api.chatbot.routes import chatbot_bp
+import os
 
 #########################################
 # Main entry point for the application.
 #########################################
 
-app = Flask(__name__,
+def create_app(config_name='default'):
+  """Application-factory pattern"""
+
+  # Selecting Flask configuration
+  app_config = config[config_name]
+
+  # Creating Flask instance
+  app = Flask(__name__,
             template_folder='kusibot/api/frontend/templates',
             static_folder='kusibot/api/frontend/static')
+  
+  # Load configuration
+  app.config.from_object(app_config)
 
-# Setting CSRF protection
-app.config['SECRET_KEY'] = 'MY_SECRET_KEY'
-csrf = CSRFProtect(app)
+  # Setting CSRF protection
+  CSRFProtect(app)
 
-# Registering the blueprints
-app.register_blueprint(main_bp)
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(chatbot_bp, url_prefix='/chatbot')
+  # Initialise database
+  init_db(app)
+
+  # Registering the blueprints
+  app.register_blueprint(main_bp)
+  app.register_blueprint(auth_bp, url_prefix='/auth')
+  app.register_blueprint(chatbot_bp, url_prefix='/chatbot')
+
+  return app
 
 
 def main():
-    """Main entry point for running the app."""
-    app.run(host="0.0.0.0", port=5000, debug=True)
+  """Main entry point for running the app."""
+
+  app = create_app(os.environ.get('FLASK_ENV', 'default'))
+  app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == '__main__':
