28071c8473b5bd5abd03c972a79b3e69d89201ad
didierrc <didieryamil02@gmail.com>
2025-03-09 20:17:52 +0100

    Fixed initial test

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,10 +1,10 @@
 import pytest
-from app import app
+from app import create_app
 
 @pytest.fixture
 def client():
     """Create a test client for the Flask app."""
-    app.config['TESTING'] = True
+    app = create_app('testing')
     with app.test_client() as client:
         yield client
 
