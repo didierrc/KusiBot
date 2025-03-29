10beede5ac59ee78bea555018e15856344638159
didierrc <didieryamil02@gmail.com>
2025-03-29 18:06:02 +0100

    Added professional user

--- kusibot/database/db.py
+++ kusibot/database/db.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+import os
 
 #########################################
 # SQLAlchemy DB initialization
@@ -25,6 +27,22 @@ def init_db(app):
     # Initialize Flask-migrate: Useful when developing for DB changes.
     migrate.init_app(app, db, directory='kusibot/database/migrations')
 
-    # Creating tables
+    # Creating tables and professional user
     with app.app_context():
+        
+        # Create all tables
         db.create_all()
+
+        # Create a professional user if not exists
+        from kusibot.database.models import User
+        professional_username = os.environ["PROFESSIONAL_USERNAME"]
+        user = User.query.filter_by(username=professional_username).first()
+        if not user:
+            hashed_password = Bcrypt().generate_password_hash(os.environ["PROFESSIONAL_PASSWORD"]).decode('utf-8')
+            user = User(username=professional_username, 
+                        email=os.getenv("PROFESSIONAL_EMAIL", "pro@kusibot.com"), 
+                        password=hashed_password,
+                        is_professional=True)
+        
+            db.session.add(user)
+            db.session.commit()
